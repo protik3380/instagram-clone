cfb914d4549dab46df139326317651920b63bf8b
Protik Hosen <protik3380@gmail.com>
2020-07-26 20:52:15 +0600

    firebase auth done

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyD3kD-54MxJSNverUqktqn1LxIrtAp9BWQ",
   authDomain: "instagram-clone-9206f.firebaseapp.com",
   databaseURL: "https://instagram-clone-9206f.firebaseio.com",
@@ -7,5 +9,9 @@ const firebaseConfig = {
   messagingSenderId: "949489988911",
   appId: "1:949489988911:web:56720fd736c0cbd4565dda",
   measurementId: "G-WVXM75M1YY",
-};
-export default firebaseConfig;
+});
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
